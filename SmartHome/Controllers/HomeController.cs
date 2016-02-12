087283431fa720da0aba4efa741cef1fda32a834
marchenkom <marchenkom@media-saturn.com>
2016-02-12 17:39:12 +0300

    Initial commit

---
+++ SmartHome/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using SmartHome.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartHome.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRootObject _root;
+        public HomeController(IRootObject root)
+        {
+            _root = root;
+        }
+
+
+        public ActionResult MainView()
+        {
+            return View(_root);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
